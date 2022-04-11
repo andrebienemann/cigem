da8481962ec87aeb447a4cf776c389293dded4c2
andrebienemann <andre.bienemann@gmail.com>
2022-04-11 19:34:25 +0200

    Extend rendering module

--- cigem/core/rendering.py
+++ cigem/core/rendering.py
@@ -5,8 +5,6 @@ from jinja2 import Environment, FileSystemLoader
 templates_path = os.path.join(os.path.dirname(__file__), "../templates")
 environment = Environment(loader=FileSystemLoader(templates_path))
 
-extension_module_template = environment.get_template("extension_module.j2")
-
 
 def render_extension_module(module):
     """
@@ -16,4 +14,19 @@ def render_extension_module(module):
     module: the module to be rendered
     """
 
+    extension_module_template = environment.get_template("extension_module.j2")
+
     return extension_module_template.render({"module": module})
+
+
+def render_setup_script(package_name):
+    """
+    Renders a setup script
+
+    Parameters
+    package_name: the name of the package
+    """
+
+    setup_script_template = environment.get_template("setup_script.j2")
+
+    return setup_script_template.render({"package_name": package_name})
