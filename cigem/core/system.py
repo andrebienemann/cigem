8c56c4d96490ea848c1bbf60c89a1a1c019452c9
andrebienemann <andre.bienemann@gmail.com>
2022-04-11 19:16:22 +0200

    Extend system module

--- cigem/core/system.py
+++ cigem/core/system.py
@@ -14,6 +14,18 @@ def call_build(path: Path):
     system(f"python3 {setup} build")
 
 
+def call_build_ext(path: Path):
+    """
+    Calls the build_ext command
+
+    Parameters
+    path: the path to the root of the package
+    """
+
+    setup = path.joinpath("setup.py")
+    system(f"python3 {setup} build_ext --inplace")
+
+
 def call_install(path: Path):
     """
     Calls the install command
@@ -24,3 +36,45 @@ def call_install(path: Path):
 
     setup = path.joinpath("setup.py")
     system(f"python3 {setup} install")
+
+
+def call_unittest(path: Path):
+    """
+    Calls the unittest command
+
+    Parameters
+    path: the path to the root of the package
+    """
+
+    tests = path.joinpath("tests")
+    system(f"python3 -m unittest discover {tests}")
+
+
+def call_mkdir(path: Path):
+    """
+    Calls the mkdir command
+
+    Parameters
+    path: the path to the root of the package
+    """
+    system(f"mkdir -p {path}")
+
+
+def call_rm(path: Path):
+    """
+    Calls the rm command
+
+    Parameters
+    path: the path to the root of the package
+    """
+    system(f"rm -r {path}")
+
+
+def call_touch(path: Path):
+    """
+    Calls the touch command
+
+    Parameters
+    path: the path to the root of the package
+    """
+    system(f"touch {path}")
