ce58658863aa9fb6892940d01d7317c6091677a0
andrebienemann <andre.bienemann@gmail.com>
2022-04-29 20:59:22 +0200

    Implement install command

--- cigem/commands/install.py
+++ cigem/commands/install.py
@@ -2,10 +2,26 @@ from pathlib import Path
 
 import click
 
+from cigem.core.rendering import render_extension_module
+from cigem.core.system import call_install, call_rm
+from cigem.model.package import Package
+
 
 @click.command(help="Install")
 @click.option(
-    "--path", type=click.Path(True), default=Path("."), help="Paths to the Package"
+    "--path",
+    required=False,
+    type=click.Path(True),
+    help="Paths to the Package",
+    default=Path("."),
 )
-def install(path: Path):
-    pass
+def install(path):
+    package = Package.make_package(path)
+
+    for module in package.modules:
+        content = render_extension_module(module)
+        path.joinpath(f"{module.name}.c").write_text(content)
+
+    call_install(path)
+
+    call_rm(path.joinpath("tmp"))
--- cigem/commands/test.py
+++ cigem/commands/test.py
@@ -2,10 +2,28 @@ from pathlib import Path
 
 import click
 
+from cigem.core.rendering import render_extension_module
+from cigem.core.system import call_build_ext, call_rm, call_unittest
+from cigem.model.package import Package
+
 
 @click.command(help="Test")
 @click.option(
-    "--path", type=click.Path(True), default=Path("."), help="Paths to the Package"
+    "--path",
+    required=False,
+    type=click.Path(True),
+    help="Paths to the Package",
+    default=Path("."),
 )
 def test(path):
-    pass
+    package = Package.make_package(path)
+
+    for module in package.modules:
+        content = render_extension_module(module)
+        path.joinpath(f"{module.name}.c").write_text(content)
+
+    call_build_ext(path)
+
+    call_unittest(path)
+
+    call_rm(path.joinpath("tmp"))
