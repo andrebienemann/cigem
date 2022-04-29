4e2abb7f6e042b291d8b44c48de3865565ba5045
andrebienemann <andre.bienemann@gmail.com>
2022-04-29 21:23:40 +0200

    Implement creat command

--- cigem/commands/create.py
+++ cigem/commands/create.py
@@ -2,10 +2,35 @@ from pathlib import Path
 
 import click
 
+from cigem.core.rendering import render_setup_script
+from cigem.core.system import call_mkdir, call_touch
+
 
 @click.command(help="Create")
 @click.option(
-    "--path", type=click.Path(True), default=Path("."), help="Paths to the Package"
+    "--name",
+    type=str,
+    required=True,
+    help="The name of the Python package to be created",
 )
-def create(path):
-    pass
+@click.option(
+    "--path",
+    required=False,
+    type=click.Path(True),
+    help="Paths to the Package",
+    default=Path("."),
+)
+def create(name, path):
+
+    project = path.joinpath(name)
+    call_mkdir(project)
+
+    module = project.joinpath(name)
+    call_mkdir(module)
+
+    init = module.joinpath("__init__.py")
+    call_touch(init)
+
+    setup = project.joinpath("setup.py")
+    content = render_setup_script(name)
+    setup.write_text(content)
