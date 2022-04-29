cf8de2bf06e3a1a325e08dc7ad5d40ef58eae454
andrebienemann <andre.bienemann@gmail.com>
2022-04-29 20:17:19 +0200

    Implement build command

--- cigem/commands/build.py
+++ cigem/commands/build.py
@@ -2,10 +2,26 @@ from pathlib import Path
 
 import click
 
+from cigem.core.rendering import render_extension_module
+from cigem.core.system import call_build, call_rm
+from cigem.model.package import Package
+
 
 @click.command(help="Build")
 @click.option(
-    "--path", type=click.Path(True), default=Path("."), help="Path to the Package"
+    "--path",
+    required=False,
+    type=click.Path(True),
+    help="Path to the Package",
+    default=Path("."),
 )
 def build(path):
-    pass
+    package = Package.make_package(path)
+
+    for module in package.modules:
+        content = render_extension_module(module)
+        path.joinpath(f"{module.name}.c").write_text(content)
+
+    call_build(path)
+
+    call_rm(path.joinpath("tmp"))
